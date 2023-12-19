dabad1a33d3c0b522f9a14548e1e3deca2ae2acf
tmnpierre <tomyn.pierre@gmail.com>
2023-12-19 20:25:55 +0100

    Initial commit

---
+++ ExpenseManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ExpenseManager
+{
+    private List<Expense> expenses;
+
+    public ExpenseManager()
+    {
+        expenses = new List<Expense>();
+    }
+
+    public void AddExpense(Expense expense)
+    {
+        expenses.Add(expense);
+    }
+
+    public void DisplayExpenses()
+    {
+        foreach (Expense e in expenses)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
